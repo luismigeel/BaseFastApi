64ac1744c1bb4245e5d8a1d183e820536a21e6d4
LuismiGeeL <zavalafelix.luismiguel@gmail.com>
2022-04-03 19:51:40 -0600

    part2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
+from asyncio.windows_events import NULL
 from fastapi import FastAPI, APIRouter
-from fastapi import status
+from fastapi import status, HTTPException
 
 
 app = FastAPI(
@@ -11,16 +12,53 @@ app = FastAPI(
 appRoute = APIRouter()
 
 
-app.include_router(appRoute)
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
+    },
+]
+
+
+@appRoute.get("/", status_code=200)
+def root() -> dict:
+    """
+    Root GET
+    """
+    return {"msg": "Hello, World!"}
 
 
 @appRoute.get(
-    path='/',
+    path='/recipe/{id}',
     status_code=status.HTTP_200_OK)
-async def root() -> dict:
+async def getRecipeById(id: int) -> dict:
     """_summary_
+
+    Args:
+        id (int): _description_
 
     Returns:
         dict: _description_
     """
-    return {'message': 'Hello World'}
+    result = [recipe for recipe in RECIPES if recipe["id"] == id]
+    if result != []:
+        return result[0]
+    raise  HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+app.include_router(appRoute)
+
